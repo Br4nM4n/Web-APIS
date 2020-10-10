d825b5a05b98a49429489721373a62945f9703ca
Br4nM4n <brandenburnett7@gmail.com>
2020-10-09 23:30:07 -0400

    week 4 homeWork

---
+++ questions.js
@@ -0,0 +1,38 @@
+var qustions = [
+ {
+ title: "Variables are used to store?",
+ choices: [ "A. values", "B. code", "C. items", "D. all the above"],
+ answer: "values";
+
+};
+{
+title: 
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+]
